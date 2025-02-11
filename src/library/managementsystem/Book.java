36ba71be5bc62edfa1c687a6c4d3952034e3e394
Nate Spencer <natespencer@mwc-057244.dhcp.missouri.edu>
2025-02-11 15:30:02 -0600

    Added default constructor and parameterized constructor

--- src/library/managementsystem/Book.java
+++ src/library/managementsystem/Book.java
@@ -8,14 +8,24 @@ public class Book {
 	// Define private attributes
 	private String title;
 	private String author;
-	private String ISB;
+	private String ISBN;
 	private double price;
 	
 	// Default Constructor
-	
+public Book() {
+	 this.title = "Unknown";
+	 this.author = "Unknown";
+	 this.ISBN = "Unknown";
+	 this.price = 0.0;
+ }
 	
 	// Parameterized Constructor
-	
+public Book(String title, String author, String ISBN, double price) {
+	this.title = title;
+	this.author = author;
+	this.ISBN = ISBN;
+	this.price = price;
+}
 	
 	// Copy Constructor
 	
