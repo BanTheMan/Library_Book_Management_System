db907caedc5750fc44302d287974c5c39b61282c
BanTheMan <brandonmgomes06@gmail.com>
2025-02-12 15:44:52 -0600

    Added more books & implemented 'Display all books' task
    Located in the main method of the LibraryApp class

--- src/library/managementsystem/app/LibraryApp.java
+++ src/library/managementsystem/app/LibraryApp.java
@@ -17,12 +17,15 @@ public class LibraryApp {
 		// Task: Add Books (min. 3)]
 		// Initialize variables
 		boolean isAdded;
-		Book[] booksToAdd = new Book[4];
+		Book[] booksToAdd = new Book[5];
 		
 		// Create books to be added
 		booksToAdd[0] = new Book();
 		booksToAdd[1] = new Book("For a New Liberty", "Murray N. Rothbard", "9781610167314", 64.68);
 		booksToAdd[2] = new Book("The Screwtape Letters", "C.S. Lewis", "9798343951851", 67.54);
+		booksToAdd[3] = new Book("ESV Church Bible Black", "Lane T. Dennis, et al.", "9781433563423", 15.04);
+		booksToAdd[4] = new Book("Understanding Deep Learning", "Simon J.D. Prince", "9780262048644", 100.00);
+		
 		
 		// Add books to library
 		for (Book book : booksToAdd) {
@@ -63,6 +66,8 @@ public class LibraryApp {
 		
 		
 		// Display all Books
+		System.out.print("All books in Library:");
+		Ellis.displayBooks();
 		
 	}
 	
