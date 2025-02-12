79ce33ad290cdd0ab1b3a22cb0328fc1b78d6130
BanTheMan <brandonmgomes06@gmail.com>
2025-02-12 14:58:15 -0600

    Implemented the 'add books to library'
    Located in the 'Main' method of the LibraryApp class.
    Creates three books and adds them to the library object.

--- src/library/managementsystem/app/LibraryApp.java
+++ src/library/managementsystem/app/LibraryApp.java
@@ -1,5 +1,6 @@
 package library.managementsystem.app;
 
+
 import library.managementsystem.Book;
 import library.managementsystem.Library;
 
@@ -8,26 +9,46 @@ import library.managementsystem.Library;
  */
 public class LibraryApp {
 	
-	// Create Library object
-	Library Ellis = new Library();
-	
-	// Add Books (min. 3)
-	Book book1 = new Book();
-	Book book2 = new Book("For a New Liberty", "Murray N. Rothbard", "9781610167314", 64.68);
-	Book book3 = new Book("The Screwtape Letters", "C.S. Lewis", "9798343951851", 67.54);
-	boolean 
-	
-	
-	// Search by ISBN & display Book
-	
-	
-	// Remove Book that exists
-	
-	
-	// Remove Book that does not exist
-	
-	
-	// Display all Books
+	public void Main() {
+		
+		// Create Library object
+		Library Ellis = new Library();
+		
+		// Task: Add Books (min. 3)]
+		// Initialize variables
+		boolean isAdded;
+		Book[] booksToAdd = new Book[3];
+		
+		// Create books to be added
+		booksToAdd[0] = new Book();
+		booksToAdd[1] = new Book("For a New Liberty", "Murray N. Rothbard", "9781610167314", 64.68);
+		booksToAdd[2] = new Book("The Screwtape Letters", "C.S. Lewis", "9798343951851", 67.54);
+		
+		// Add books to library
+		for (Book book : booksToAdd) {
+			// Add book
+			isAdded = Ellis.addBook(book);
+			// Check success
+			if (isAdded) { 
+				System.out.println("Successfully added " + book.getTitle()); 
+			} else {
+				System.out.println("Failed to add " + book.getTitle());
+			}
+		}
+		
+		
+		// Task: Search by ISBN & display Book
+		
+		
+		// Task: Remove Book that exists
+		
+		
+		// Task: Remove Book that does not exist
+		
+		
+		// Display all Books
+		
+	}
 	
 	
 }
